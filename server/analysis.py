849f6135daa465ef6f46cf246c00282cf210168d
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-03-04 22:09:15 +0800

    Implemented line densification & line explosion + quick test

--- server/analysis.py
+++ server/analysis.py
@@ -1,6 +1,9 @@
 import geopandas as gpd
 from sqlalchemy import create_engine
 import rasterio as rio
+from shapely.geometry import LineString, MultiLineString
+
+# --------- DATA INGESTION ----------- #
 
 # Database connection parameters
 host = "localhost"
@@ -26,6 +29,42 @@ roads = roads.set_crs(epsg=3123, allow_override=True)
 
 dem = rio.open("data/dem.tif")
 
-print("DEM CRS:", dem.crs)
-print("DEM Resolution:", dem.res)
-print("DEM Bounds:", dem.bounds)
+# print("DEM CRS:", dem.crs)
+# print("DEM Resolution:", dem.res)
+# print("DEM Bounds:", dem.bounds)
+
+# ---------- ELEVATION SAMPLING and ----------#
+# ---------- LINESTRINGZ CONSTRUCTION ------- #
+
+SAMPLE_STEP = 10    # meters (adjust later for sensitivity)
+
+
+def densify_line(line: LineString, step: float):
+    """Return points sampled along a line at fixed distance spacing."""
+    if line.length == 0:
+        return []
+    
+    distances = list(range(0, int(line.length), int(step)))
+
+    pts = [line.interpolate(d) for d in distances]
+    pts.append(line.interpolate(line.length))
+    
+    return pts
+
+
+def explode_to_lines(geom):
+    """Explode MultiLineString features into LineString."""
+    match geom.geom_type:
+        case "LineString":
+            return [geom]
+        case "MultiLineString":
+            return list(geom.geoms)
+        case _:
+            return []
+        
+
+# Quick test of the sampling function on the first road geometry
+test_geom = roads.geometry.iloc[0]
+lines = explode_to_lines(test_geom)
+pts = densify_line(lines[0], SAMPLE_STEP)
+print("Sample points:", len(pts), "Line length:", lines[0].length)
